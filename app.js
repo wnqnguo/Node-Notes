7793050fb97127ad6ce34fec054bb6c7bce42afc
Admin <admin@Admins-MacBook-Pro.local>
2016-11-08 11:26:18 -0800

    test commit

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('starting app.');
+
+const fs = require('fs');
+const _ = require('lodash');
+const notes = require('./notes.js');
+
+
+
---
+++ notes.js
@@ -0,0 +1,4 @@
+module.exports.addNotes =()=>{
+	console.log('addNote');
+	return 'New note'
+}
